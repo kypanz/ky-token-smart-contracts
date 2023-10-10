286bf2db17bc85d4c041e597c98204677ab1596d
kypanz <kypanz95@gmail.com>
2023-10-10 16:45:11 -0300

    formatting document

--- test/Ky-Token20.ts
+++ test/Ky-Token20.ts
@@ -14,17 +14,17 @@ async function deployContract() {
 
 // This function get the balance in string format
 function b(balance) {
-  console.log('tipo recibido => ',typeof balance);
+  console.log('tipo recibido => ', typeof balance);
   return ethers.formatEther(balance);
 }
 
 
 describe("Ky-Token", function () {
 
-  let contractToken : Signer;
-  let owner : Signer;
-  let accOne : Signer;
-  let accTwo : Signer;
+  let contractToken: Signer;
+  let owner: Signer;
+  let accOne: Signer;
+  let accTwo: Signer;
 
   beforeEach(async () => {
     const { contract, accountOwner, accountOne, accountTwo } = await deployContract();
@@ -44,7 +44,7 @@ describe("Ky-Token", function () {
       expect(ethers.formatEther(balanceAccountOne)).to.be.equal('0.0');
       expect(ethers.formatEther(balanceAccountTwo)).to.be.equal('0.0');
     });
-  
+
     // TODO : Create tests for the transactions with frees in ERC20 ;)
     it("Sending tokens", async function () {
       const response = await contractToken.transfer(accOne.address, 10);
@@ -56,7 +56,7 @@ describe("Ky-Token", function () {
       // TODO : Calculate the receive amount - tax percentage ;)
       //expect(response.to).to.be.equal(contractToken.address);
     });
-  
+
   });
 
 
